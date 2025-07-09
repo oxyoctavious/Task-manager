c2c6870967fb0716f99e04c14f40a1973ace2ebb
Your GitHub Username <vedantbvidhate@gmail.com>
2025-07-09 17:29:28 +0530

    Task manager

---
+++ main.py
@@ -0,0 +1,56 @@
+import json
+import os
+
+
+TASK_FILE = "tasks.json"
+
+
+def load_tasks():
+    """Load task from JSON file."""
+    if not os.path.exists(TASK_FILE):
+        return []
+    with open(TASK_FILE, "r") as file:
+        return json.load(file)
+    
+def save_tasks(tasks):
+    """Save tasks to the JSON file."""
+    with open(TASK_FILE, "w") as file:
+        json.dump(tasks, file, indent=4)
+        
+        
+def show_tasks(tasks):
+    """Display all tasks."""
+    if not tasks:
+        print("No tasks available.")
+        return   
+    print("\n Your Tasks:")
+    for idx, task in enumerate(tasks, start=1):
+        status = "Done" if task['done'] else "Not Done"
+        print(f"{idx}. [{status}] {task['title']}")
+        
+# Main menu Loop
+def main():
+    tasks = load_tasks()
+    while True:
+        print("\nTask Manager")
+        print("1. View Tasks")
+        print("2. Add Task")
+        
+        print("3. Exit")
+        
+        
+        choice = input("Choose an option: ")
+        if choice == "1":
+            show_tasks(tasks)
+        elif choice == "2":
+            title = input("Enter task title: ")
+            tasks.append({"title": title, "done": False})
+            save_tasks(tasks)
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice.")
+            
+            
+if __name__ == "__main__":
+    main()
